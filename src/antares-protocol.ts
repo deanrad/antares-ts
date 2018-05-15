033af8157b9c1e1eb581d987abc42385f8acb8cc
Dean Radcliffe <deanmisc@yahoo.com>
2018-05-15 16:46:04 -0500

    feat: Introduce process, subscribeRenderer methods to AntaresProtocol

--- src/antares-protocol.ts
+++ src/antares-protocol.ts
@@ -1,4 +1,26 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class AntaresProtocol {}
+
+export interface Action {
+  type: string
+  payload?: any
+  error?: boolean
+  meta?: Object
+}
+
+export interface ActionStreamItem {
+  action: Action
+}
+
+export interface Renderer {
+  (item: ActionStreamItem): any
+}
+
+export default class AntaresProtocol {
+  process(action: Action): Promise<any> {
+    return Promise.resolve('☸')
+  }
+  subscribeRenderer(renderer: Renderer): void {
+    // HACK eventually the renderer will get called for every processed action
+    renderer({ action: { type: 'Persist.toDB' } })
+  }
+}
--- test/antares-protocol.test.ts
+++ test/antares-protocol.test.ts
@@ -3,12 +3,40 @@ import AntaresProtocol from '../src/antares-protocol'
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+describe('AntaresProtocol', () => {
+  it('is instantiable', () => {
+    expect(new AntaresProtocol()).toBeInstanceOf(AntaresProtocol)
+  })
+  it('has process, subscribeRenderer methods', () => {
+    const antares = new AntaresProtocol()
+    expect(antares).toMatchObject({
+      process: expect.any(Function),
+      subscribeRenderer: expect.any(Function)
+    })
   })
 
-  it('AntaresProtocol is instantiable', () => {
-    expect(new AntaresProtocol()).toBeInstanceOf(AntaresProtocol)
+  describe('instance methods', () => {
+    let antares
+    beforeEach(() => {
+      antares = new AntaresProtocol()
+    })
+
+    describe('#process', () => {
+      it('accepts an action, and returns a promise', () => {
+        const result = antares.process({ type: 'SomeAction' })
+        expect(result).toBeInstanceOf(Promise)
+      })
+    })
+
+    describe('#subscribeRenderer', () => {
+      it('accepts a function to be called upon processing of actions', () => {
+        const renderer = jest.fn()
+        const result = antares.subscribeRenderer(renderer)
+        const action = { type: 'XXX' }
+
+        // XXX should assert the actual action goes through once we have an actual RxJS Subject to subscribe to
+        expect(renderer).toHaveBeenCalledWith({ action: expect.any(Object) })
+      })
+    })
   })
 })
